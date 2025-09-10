17d78d473ad1a0ce0d3cebb091c5f72eb807be5d
as00000000 <anitsarkar04@gmail.com>
2025-09-10 22:23:51 +0530

    demo_frontend

---
+++ next.config.js
@@ -0,0 +1,31 @@
+// /**
+//  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+//  * for Docker builds.
+//  */
+// import "./src/env.js";
+
+// /** @type {import("next").NextConfig} */
+// const config = {};
+
+// export default config;
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
+};
+
+export default config;
